ae983b07f58b34ee02e3e35677faf7b4d5510a8c
Antonie Lin <iammrhelo@gmail.com>
2018-11-06 04:56:38 +0000

    add bpe 20k 25k

---
+++ assignment2/data.bpe2/process_vocab.sh
@@ -0,0 +1,17 @@
+
+#
+#!/bin/bash
+
+lowresource=('az' 'be' 'gl');
+highresource=('tr' 'ru' 'pt');
+
+cd ..
+for ((idx=0; idx<${#lowresource[@]}; ++idx)); 
+do
+    l1=${lowresource[idx]}; 
+    l2=${highresource[idx]};
+    echo Processing $l1+$l2
+    echo "python vocab.py --train-src=./data.bpe2/train.en-$l1+$l2.$l1+$l2.bpe.txt --train-tgt=./data.bpe2/train.en-$l1+$l2.en.bpe.txt ./data.bpe2/vocab.$l1+$l2-en.bpe.bin"
+    python vocab.py --train-src=./data.bpe2/train.en-$l1+$l2.$l1+$l2.bpe.txt --train-tgt=./data.bpe2/train.en-$l1+$l2.en.bpe.txt ./data.bpe2/vocab.$l1+$l2-en.bpe.bin
+done;
+
---
+++ assignment2/data.bpe3/process_vocab.sh
@@ -0,0 +1,17 @@
+
+#
+#!/bin/bash
+
+lowresource=('az' 'be' 'gl');
+highresource=('tr' 'ru' 'pt');
+
+cd ..
+for ((idx=0; idx<${#lowresource[@]}; ++idx)); 
+do
+    l1=${lowresource[idx]}; 
+    l2=${highresource[idx]};
+    echo Processing $l1+$l2
+    echo "python vocab.py --train-src=./data.bpe3/train.en-$l1+$l2.$l1+$l2.bpe.txt --train-tgt=./data.bpe3/train.en-$l1+$l2.en.bpe.txt ./data.bpe3/vocab.$l1+$l2-en.bpe.bin"
+    python vocab.py --train-src=./data.bpe3/train.en-$l1+$l2.$l1+$l2.bpe.txt --train-tgt=./data.bpe3/train.en-$l1+$l2.en.bpe.txt ./data.bpe3/vocab.$l1+$l2-en.bpe.bin
+done;
+
