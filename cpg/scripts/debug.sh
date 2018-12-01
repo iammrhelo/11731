2e6fe785df7a92ea0851da9fda31129fd2892740
Antonie Lin <iammrhelo@gmail.com>
2018-12-01 17:29:00 -0500

    add cpg skeleton

---
+++ cpg/scripts/debug.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+vocab="debug/vocab.debug.bin"
+train_src_list="debug/train_src_list.txt"
+train_tgt_list="debug/train_tgt_list.txt"
+dev_src_list="debug/valid_src_list.txt"
+dev_tgt_list="debug/valid_tgt_list.txt"
+
+work_dir="work_dir.debug"
+
+mkdir -p ${work_dir}
+echo save results to ${work_dir}
+
+batch_size=3
+valid_niter=2400
+
+echo batch_size ${batch_size}
+python cpg_skeleton.py \
+    train \
+    --cuda \
+    --vocab ${vocab} \
+    --train-src ${train_src_list} \
+    --train-tgt ${train_tgt_list} \
+    --dev-src ${dev_src_list} \
+    --dev-tgt ${dev_tgt_list} \
+    --save-to ${work_dir} \
+    --num-layers 1 \
+    --max-epoch 10 \
+    --valid-niter ${valid_niter} \
+    --batch-size ${batch_size} \
+    --hidden-size 50 \
+    --embed-size 50 \
+    --uniform-init 0.1 \
+    --dropout 0.0 \
+    --clip-grad 5.0 \
+    --lr-decay 0.5 \
+    --bidirectional  \
+
+#>${work_dir}/err.log
+
